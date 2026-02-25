34fe60d1353dcf3b7943a426a8986be1435c92ef
ilya <kireevilia1@mail.ru>
2026-02-25 14:31:11 +0300

    TO DO: ref

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-# main.py
 import cv2
 from camera_manager import CameraManager
 from stereo_sgbm import StereoSGBM
 from detector import ObjectDetector
 from fps_counter import FPSCounter
 import utils
+import numpy as np
 
 def main():
     cam = CameraManager().start()
     real_focal, real_baseline = cam.get_calibration()
 
-    depth_engine = StereoSGBM(scale=1, use_wls=True)
+    depth_engine = StereoSGBM(scale=0.6, use_wls=True)
     
     detector = ObjectDetector()
     
     fps = FPSCounter()
     
+    width_depth = 640
+    height_depth = 400
+    cx = width_depth / 2
+    cy = height_depth / 2
+
     try:
         while True:
             left, right, rgb = cam.get_frames()
@@ -32,8 +37,8 @@ def main():
             cv2.putText(rgb, fps.get_fps_text(), (10, 100), 
                         cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 5)
             
-            cv2.imshow("RGB + YOLO", cv2.resize(rgb, (640, 360)))
-            cv2.imshow("SGBM Depth", cv2.resize(depth_vis, (640, 360)))
+            cv2.imshow("Main", cv2.resize(rgb, (640, 360)))
+            cv2.imshow("Depth", cv2.resize(depth_vis, (640, 360)))
             
             if cv2.waitKey(1) == ord('q'):
                 break
